cac20e6abb17a70f262650cf06578dddc313aa37
liambut <liampb@outlook.com>
2022-10-11 19:15:21 +0100

    fixed formatter issue

--- invoice/Invoice.java
+++ invoice/Invoice.java
@@ -60,13 +60,6 @@ public class Invoice {
     return formattedInvoice;
   }
 
-  // DELETE!!!!!!!
-  public double totalPrice() {
-    PriceCalculator calculator = new PriceCalculator();
-    accept(calculator);
-    return calculator.getTotalPrice();
-  }
-
   public void accept(ItemVisitor visitor) {
     Iterator<Item> iter = getItems();
     for(int i=0;i< items.size();i++){
